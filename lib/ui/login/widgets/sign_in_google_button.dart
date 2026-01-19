3925e4d675605231761b854da04f1bc9bfa11324
Alisson Amarante <alisson.amarante@ccc.ufcg.edu.br>
2026-01-19 10:42:16 -0300

    feat(login): implement login screen UI base on figma design

---
+++ lib/ui/login/widgets/sign_in_google_button.dart
@@ -0,0 +1,34 @@
+import 'package:cinebox_app/ui/core/themes/colors.dart';
+import 'package:cinebox_app/ui/core/themes/resource.dart';
+import 'package:cinebox_app/ui/core/themes/text_style.dart';
+import 'package:flutter/material.dart';
+
+class SignInGoogleButton extends StatelessWidget {
+
+  const SignInGoogleButton({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          padding: EdgeInsets.zero,
+        ),
+        onPressed: (){}, 
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Image.asset(R.ASSETS_IMAGES_GOOGLE_LOGO_PNG
+              ),
+            ),
+            const Text(
+              'Entrar com Google',
+              style: TextStyle(fontSize: 14, color: AppColors.darkGrey),
+            ),
+          ],
+        ),
+        );
+  }
+}
