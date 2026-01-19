09eaa5dc06f7dda8e4290341259ffc15f3f21d16
Alisson Amarante <alisson.amarante@ccc.ufcg.edu.br>
2026-01-19 13:01:05 -0300

    feat(auth): complete login flow and implement home redirection on success

--- lib/cinebox_main_app.dart
+++ lib/cinebox_main_app.dart
@@ -1,4 +1,5 @@
 import 'package:cinebox_app/ui/core/themes/theme.dart';
+import 'package:cinebox_app/ui/home/home_screen.dart';
 import 'package:cinebox_app/ui/login/login_screen.dart';
 import 'package:cinebox_app/ui/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,7 @@ class CineboxMainApp extends StatelessWidget {
         routes: {
           '/': (_) => SplashScreen(),
           '/login': (_) => LoginScreen(),
+          '/home': (_) => const HomeScreen(),
         },
        );
   }
