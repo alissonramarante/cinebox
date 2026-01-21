464f953da4a80d85cfb9f053faaf4857ec45f056
Alisson Amarante <alisson.amarante@ccc.ufcg.edu.br>
2026-01-21 09:06:26 -0300

    feat(hom): add movies by search and movies by gender widgets

--- lib/ui/movies/movies_screen.dart
+++ lib/ui/movies/movies_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cinebox_app/ui/movies/widgets/genres_box.dart';
 import 'package:cinebox_app/ui/movies/widgets/movies_appbar.dart';
 import 'package:cinebox_app/ui/movies/widgets/movies_by_category.dart';
+import 'package:cinebox_app/ui/movies/widgets/movies_by_gender.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,7 +26,8 @@ class _MoviesScreenState extends ConsumerState<MoviesScreen> {
             ),
         ),
         SliverToBoxAdapter(
-          child: MoviesByCategory(),
+          //child: MoviesByCategory(),
+          child: MoviesByGender(),
         )
       ]
     );
